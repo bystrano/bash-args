712c0ba4d596febf3fe67863b9e48125918b165d
Michel Bystranowski <bystrano@gmx.ch>
2020-04-11 16:20:19 +0200

    translate old comments

--- lib/out.sh
+++ lib/out.sh
@@ -3,32 +3,32 @@ set -euo pipefail
 
 out_usage_error () {
 
-    # texte en jaune
+    # text in yellow
     echo "$(tput setaf 3)$1$(tput sgr 0)" 1>&2;
     exit 2;
 }
 
 out_fatal_error () {
 
-    # texte en rouge
+    # text in read
     echo "$(tput setaf 1)$1$(tput sgr 0)" 1>&2;
     exit 1;
 }
 
 out_exec () {
 
-    # la description en vert s'il y en a une
+    # the summary in green
     if [[ -n "$1" ]]; then
         echo "$(tput setaf 2)# $1$(tput sgr 0)"
     fi
     shift
     if [[ ${dry_run:-0} -ne 1 ]]; then
-        # la commande en bleu
+        # the command in blue
         echo "$(tput setaf 4)> $*$(tput sgr 0)"
-        # exécuter la commande
+        # execute the command
         eval "$*"
     else
-        # la commande en bleu dimmé
+        # the command in dimmed blue
         echo "$(tput dim)$(tput setaf 4)> $*$(tput sgr 0)"
     fi
 }
