03dad943713a5f4c2c94842bacef50cb3e83f19b
Michel Bystranowski <bystrano@gmx.ch>
2020-04-14 13:21:45 +0200

    linting

--- lib/meta.sh
+++ lib/meta.sh
@@ -13,13 +13,12 @@ _meta_get () {
 _meta_get_option () {
     local options
 
-    # shellcheck disable=SC2154
     options="$( _meta_get "$1" "options" )"
     echo "$options" | awk -v option="$2" -v param="$3" -f "${CMD_DIR:=.}"/lib/meta_get_option.awk
 }
 
 _meta_command_get () {
 
-    # shellcheck disable=SC2154
+    # shellcheck disable=SC2153
     _meta_get "$SCRIPT_DIR/${CMDS_DIR}/${1}.sh" "$2"
 }
